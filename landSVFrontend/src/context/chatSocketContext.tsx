97566683703d6e029857077414af1fee8e02fba6
DiegoDeveloperSv <diegocornejo958@gmail.com>
2026-02-06 14:55:22 -0600

    socket

--- landSVFrontend/src/context/chatSocketContext.tsx
+++ landSVFrontend/src/context/chatSocketContext.tsx
@@ -10,7 +10,7 @@ import { toast } from "sonner";
 
 interface ChatSocketContexType {
     socket: WebSocket | null,
-    sendMessage: (msg:MessageToSend) => void
+    sendMessage: (msg: MessageToSend) => void
 }
 
 export const ChatSocketContext = createContext<ChatSocketContexType | undefined>(undefined)
@@ -20,62 +20,56 @@ export const ChatSocketProvider: React.FC<{ children: React.ReactNode }> = ({ ch
     const { user } = useUser()
     const { chat } = useSelectedChat()
     const dispatch = useDispatch<AppDispatch>()
-    
-    const selectedChatRef = useRef(chat) 
+
+    const selectedChatRef = useRef(chat)
 
     useEffect(() => {
         selectedChatRef.current = chat
-    }, [chat]) 
+    }, [chat])
 
     useEffect(() => {
-        if(!user) return
+        if (!user) return
 
-        const protocol = window.location.protocol === 'https' ? 'wss' : 'ws'
-
-        const ws = new WebSocket(`${protocol}://${window.location.host}/ws/chats?userId=${user?.id}`)
+        const ws = new WebSocket(`api/ws/chats?userId=${user?.id}`)
         socket.current = ws
 
-        ws.onopen = () => console.log('ready for use the socket')
-        ws.onerror = () => console.error('an error ocurred with the socket')
-        ws.onclose = () => console.warn('socket conection closed')
-        
         ws.onmessage = (event) => {
             const message: Message = JSON.parse(event.data)
-            
-            switch(message.type) {
-                case 'PRIVATE' :
 
-                dispatch(addMessageToChat(message))
+            switch (message.type) {
+                case 'PRIVATE':
 
-                if(message.chatId === selectedChatRef.current?.id) { 
-                    dispatch(addMessage(message))
-                }
+                    dispatch(addMessageToChat(message))
 
-                if(selectedChatRef?.current?.id === message.chatId && user.id != message.senderId) {
-                    const destinatary = selectedChatRef.current.creatorId === user.id ? selectedChatRef.current.invitedId : selectedChatRef.current.creatorId
-                    const message: MessageToSend = {
-                        receiverId: destinatary,
-                        senderId: user.id,
-                        content: selectedChatRef.current.id,
-                        createdAt: new Date().toISOString(),
-                        type: 'SEEN_CHAT'
+                    if (message.chatId === selectedChatRef.current?.id) {
+                        dispatch(addMessage(message))
                     }
 
-                    sendMessage(message)
-                }
-                break;
-                
+                    if (selectedChatRef?.current?.id === message.chatId && user.id != message.senderId) {
+                        const destinatary = selectedChatRef.current.creatorId === user.id ? selectedChatRef.current.invitedId : selectedChatRef.current.creatorId
+                        const message: MessageToSend = {
+                            receiverId: destinatary,
+                            senderId: user.id,
+                            content: selectedChatRef.current.id,
+                            createdAt: new Date().toISOString(),
+                            type: 'SEEN_CHAT'
+                        }
+
+                        sendMessage(message)
+                    }
+                    break;
+
                 case 'NOTIFICATION':
                     toast.success(message.content)
                     break;
-                
-                case 'SEEN_CHAT' :
+
+                case 'SEEN_CHAT':
                     dispatch(setHasUnseenMessage({ id: message.chatId }))
                     break
-                
-                case 'DELETE_CHAT' : 
+
+                case 'DELETE_CHAT':
                     dispatch(deleteChat({ id: message.chatId }))
-                    if(message.chatId === selectedChatRef.current?.id) { dispatch(setSelectedChat(null)) }
+                    if (message.chatId === selectedChatRef.current?.id) { dispatch(setSelectedChat(null)) }
                     break
             }
         }
@@ -84,7 +78,7 @@ export const ChatSocketProvider: React.FC<{ children: React.ReactNode }> = ({ ch
     }, [user, dispatch])
 
     const sendMessage = (message: MessageToSend) => {
-        if(socket.current?.readyState === WebSocket.OPEN) {
+        if (socket.current?.readyState === WebSocket.OPEN) {
             socket.current.send(JSON.stringify(message))
         } else {
             console.error("No se pudo enviar el mensaje, el socket no está abierto (estado:", socket.current?.readyState, ")")
@@ -92,7 +86,7 @@ export const ChatSocketProvider: React.FC<{ children: React.ReactNode }> = ({ ch
     }
 
     return (
-        <ChatSocketContext.Provider value={{socket: socket.current, sendMessage}}>
+        <ChatSocketContext.Provider value={{ socket: socket.current, sendMessage }}>
             {children}
         </ChatSocketContext.Provider>
     )
